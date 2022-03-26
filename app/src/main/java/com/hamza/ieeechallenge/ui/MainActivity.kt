1bcdbce91d43a77aa54ba06351b5a758a9f03f66
barmg3 <barmg3@gmail.com>
2022-03-26 19:23:50 +0200

    Create AccountSetup Screens

--- app/src/main/java/com/hamza/ieeechallenge/ui/MainActivity.kt
+++ app/src/main/java/com/hamza/ieeechallenge/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.hamza.ieeechallenge
+package com.hamza.ieeechallenge.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hamza.ieeechallenge.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/hamza/ieeechallenge/ui/welcome/SplashFragment.kt
@@ -0,0 +1,24 @@
+package com.hamza.ieeechallenge.ui.welcome
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.hamza.ieeechallenge.R
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        Handler().postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_welcomeFragment)
+        },1000)
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+}
